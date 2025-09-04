d3f6b926b36d69bd25a86c1bfedbe70ee051031c
dreadedd <obayim@st.ug.edu.gh>
2025-09-04 14:57:39 +0000

    Update app.py
    final message

--- app.py
+++ app.py
@@ -1,22 +1,3 @@
-import tkinter as tk
-
-root = tk.Tk()
-root.title("Record Manager")
-root.geometry("600x500")  # Enlarged window
-root.configure(bg="#e6f7ff")
-
-tk.Label(root, text="Record Manager App", font=("Arial", 18, "bold"), bg="#e6f7ff").pack(pady=10)
-
-root.mainloop()
-
-import tkinter as tk
-
-root = tk.Tk()
-root.title("Record Manager")
-root.geometry("600x500")  # Enlarged window
-root.configure(bg="#e6f7ff")
-
-tk.Label(root, text="Record Manager App", font=("Arial", 18, "bold"), bg="#e6f7ff").pack(pady=10)
-
-root.mainloop()
+Stacy is here
+print (''Stacy is here'')
 
