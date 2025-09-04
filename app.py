fdefb444bed2089ce138792b45ea362c1ea1571f
Obed Boateng Ayim <obedb5185@gmai.com>
2025-09-04 14:45:12 +0000

    example

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ print("Titus is done. ")
 print("Obed changed this file ")
 print("Titus is done. ")
 print("gilbert likes eating. ")
-groupleader = "group leader"
+group_leader = "group leader"
 stacy = True 
 print ("Stacy is here ")
-valary = True
-print("Valary is here ")
+present = True
+print("Obed is here ")
